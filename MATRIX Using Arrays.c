631e579ae12d4986f07fc36635dc91d88e8470b4
manojsai7 <144029171+manojsai7@users.noreply.github.com>
2023-09-11 20:24:26 +0530

    Arrays in C

---
+++ MATRIX Using Arrays.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+ int x[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+ for (int i = 0; i < 3; i++)
+ {
+    for (int j = 0; j < 3; j++)
+   {
+      printf(" Element of (%d,%d) is ",i,j);
+      printf("%d ",x[i][j]);
+   }
+// for (int i = 0; i < 3; i++)
+// {
+//    for (int j = 0; j < 3; j++)
+//    {
+//    printf("%d ",x[i][j]);
+//    }
+  
+// }
+printf(" \n");
+   
+   
+ }
+ 
+
+    return 0;
+}
